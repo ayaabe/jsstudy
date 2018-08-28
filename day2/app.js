d19358f7c501bf2926007ad68b003952457d0669
tacogips <me@tacogips.me>
2018-08-28 18:46:34 +0900

    some

--- day2/app.js
+++ day2/app.js
@@ -2,12 +2,13 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+app.use('/', express.static(path.join(__dirname)))
 var server = app.listen(3000, function() {
   console.log("app listening at :3000")
 })
 
 
-app.get("/", function(req, res, next) {
-  res.sendFile(path.join(__dirname, 'index.html'));
-})
+//app.get("/", function(req, res, next) {
+//  res.sendFile(path.join(__dirname, 'index.html'));
+//})
 
